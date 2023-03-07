a645a750a5fa04c24577771625f9d04350cc6a1f
Puyan Wei <puyanwei@gmail.com>
2023-03-07 12:42:26 +0000

    added custom colors according to style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: "#26c0ab",
+        "dark-cyan": "#00494d",
+        "gray-cyan": {
+          50: "#f4fafa",
+          100: "#c5e4e7",
+          300: "#7f9c9f",
+          500: "#5e7a7d",
+        },
+      },
+    },
   },
   plugins: [],
 }
