9231a7984ad2c63143bff2dd97f10e7867d0c6ae
Puyan Wei <puyanwei@gmail.com>
2023-03-07 13:23:51 +0000

    update font setup with tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { Space_Mono } from "next/font/google"
+
+const spaceMono = Space_Mono({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={spaceMono.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default MyApp
