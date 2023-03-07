fc45570a25fc852c585b4a7fbf8efc977a0eeebb
Puyan Wei <puyanwei@gmail.com>
2023-03-07 12:41:52 +0000

    added custom colors according to designs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next"
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline bg-blue-600">Does Tailwind work?</h1>
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold text-white">White</h1>
+      <h1 className="text-3xl font-bold text-gray-cyan-50">Very Light Grayish Cyan</h1>
+      <h1 className="text-3xl font-bold text-gray-cyan-100">Light Grayish Cyan</h1>
+      <h1 className="text-3xl font-bold text-gray-cyan-300">Grayish Cyan</h1>
+      <h1 className="text-3xl font-bold text-gray-cyan-500">Dark Grayish Cyan</h1>
+      <h1 className="text-3xl font-bold text-dark-cyan">Very Dark Cyan</h1>
+      <h1 className="text-3xl font-bold text-cyan">Cyan</h1>
+    </div>
+  )
 }
 
 export default Home
