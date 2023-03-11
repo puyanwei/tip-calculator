cea5bcbfab8ee0b07b3f443016753bf4b1a5aa41
Puyan Wei <puyanwei@gmail.com>
2023-03-11 12:00:44 +0000

    add logo assets, position Splitter logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import type { NextPage } from "next"
+import Image from "next/image"
 import { Calculator } from "../components/Calculator"
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-gray-cyan-100 text-dark-cyan">
-      <h1>Splitter Title</h1>
+    <div className="bg-gray-cyan-100 text-gray-cyan-500 font-semibold">
+      <Image
+        className="mx-auto py-8"
+        src="/assets/logo.svg"
+        width="100"
+        height="100"
+        alt="Tip Splitter logo"
+      />
+      <h1 className="hidden">Tip Splitter</h1>
       <Calculator />
     </div>
   )
